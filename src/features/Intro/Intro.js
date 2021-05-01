3501e0f4a4797fa113d6b8b02279617c3410ed6f
rodolphe37 <rodolphe.a@gmail.com>
2021-05-01 11:54:33 +0200

    delete tetris link

--- src/features/Intro/Intro.js
+++ src/features/Intro/Intro.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './intro.css'
+import "./intro.css";
 import Car from "./components/Car/Car";
 import Bubble from "./components/Bubble/Bubble";
 import House from "./components/House/House";
@@ -13,12 +13,11 @@ import { useImagesContext } from "../../contexts/ImagesContext";
 import { CarWithBubble, Bottom } from "./styles";
 import Header from "./Header";
 
-
-export default props => {
+export default (props) => {
   const [selectedGame, setSelectedGame] = useState(false);
   const { images } = useImagesContext();
 
-  const selectGameHandler = game => {
+  const selectGameHandler = (game) => {
     setSelectedGame(true);
     setTimeout(() => props.history.push("/" + game), 1500);
   };
@@ -38,13 +37,40 @@ export default props => {
 
       <Bottom>
         <div className="bottom-link" style={{ display: "float" }}>
-          <img src={images["stars.svg"]} alt="github" style={{ width: 20 }} />Disponible pour PC, Mac et Linux sur<a href="https://github.com/rodolphe37/install-games-repository/blob/master/README.md" title="Jeux disponibles pour Windows, Mac et Linux" target="_blank" rel="noopener noreferrer">github</a>{" "}
-          <img src={images["stars.svg"]} alt="github" style={{ width: 20 }} />Mes produits dérivés sur<a href="https://www.redbubble.com/fr/people/rodolphe-a/shop?asc=u&ref=account-nav-dropdown" title="Mes produits dérivés" target="_blank" rel="noopener noreferrer">RedBubble</a>{" "}
-          <img src={images["stars.svg"]} alt="github" style={{ width: 20 }} />Mon Livre 42 Postures de Yoga sur<a href="https://www.thebookedition.com/fr/42-postures-de-yoga-et-etirements-p-368479.html" title="Mon Livre 42 Postures de Yoga et étirements" target="_blank" rel="noopener noreferrer">The Book Edition</a>{" "}
+          <img src={images["stars.svg"]} alt="github" style={{ width: 20 }} />
+          Disponible pour PC, Mac et Linux sur
+          <a
+            href="https://github.com/rodolphe37/install-games-repository/blob/master/README.md"
+            title="Jeux disponibles pour Windows, Mac et Linux"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>{" "}
+          <img src={images["stars.svg"]} alt="github" style={{ width: 20 }} />
+          Mes produits dérivés sur
+          <a
+            href="https://www.redbubble.com/fr/people/rodolphe-a/shop?asc=u&ref=account-nav-dropdown"
+            title="Mes produits dérivés"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            RedBubble
+          </a>{" "}
+          <img src={images["stars.svg"]} alt="github" style={{ width: 20 }} />
+          Mes livres sur
+          <a
+            href="https://www.thebookedition.com/fr/35750_rodolphe-augusto"
+            title="Mon Livre 42 Postures de Yoga et étirements"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The Book Edition
+          </a>{" "}
         </div>
-        <style jsx='true'>{`
+        <style jsx="true">{`
           .buttonLanguage {
-            left: 25%
+            left: 25%;
           }
         `}</style>
       </Bottom>
