8a1c7c87cff3fd5fa1c2f7289c58a5feabf4c6ca
rodolphe37 <rodolphe.a@gmail.com>
2021-05-01 13:23:13 +0200

    delete tetris link

--- src/features/AsanasGuessing/HeaderGuess.js
+++ src/features/AsanasGuessing/HeaderGuess.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const HeaderGuess = () => {
-  const { API_GAME } = process.env
+  const { API_GAME } = process.env;
   return (
     <div className="header-guess">
       {/*<img className="back-board" src={require('./ressources/lotus-background.png')} alt="" />*/}
       <h1>Asanas Guessing Game</h1>
       <p>By Rodolphe Augusto</p>
-      <button className="return-asanas shake-horizontal"><a href={`${API_GAME}`} onClick={() => API_GAME} alt="">Retour à L'acceuil des jeux</a></button>
+      <button className="return-asanas shake-horizontal">
+        <a href="/" onClick={() => API_GAME} alt="">
+          Retour à L'acceuil des jeux
+        </a>
+      </button>
       {/*<img className="lotus-bottom" src={require('./ressources/pink-lotus-flower.png')} alt="" />*/}
     </div>
-  )
-}
+  );
+};
 
 export default HeaderGuess;
